e99d2ca4732a51b0277157e9afa10992c7c82da8
Reece <reecemorgandev@gmail.com>
2024-06-23 16:14:55 +0100

    Create ToDo List component

---
+++ src/components/todo-list.tsx
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../store/to-dos/to-dos";
+
+function ToDoList() {
+  const dispatch = useDispatch();
+  const { todos } = useAppSelector((state) => state.ToDos);
+
+  const markAsCompleted = (todo: string) => {
+    dispatch(removeTodo(todo));
+  };
+
+  return (
+    <Wrapper>
+      <Title>Your ToDo List</Title>
+      {todos.length === 0 ? (
+        <NoToDos>
+          <p>Your To Do List is currently empty</p>
+          <p>Add a new ToDo above</p>
+        </NoToDos>
+      ) : (
+        <ToDos>
+          <List>
+            {todos.map((todo, i) => (
+              <ListItem key={i}>
+                <ToDoItem>{todo}</ToDoItem>
+                <Button onClick={() => markAsCompleted(todo)}>
+                  Mark as Completed
+                </Button>
+              </ListItem>
+            ))}
+          </List>
+        </ToDos>
+      )}
+    </Wrapper>
+  );
+}
+
+export default ToDoList;
+
+const Wrapper = styled.section`
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+const NoToDos = styled.section``;
+
+const ToDos = styled.section``;
+
+const Title = styled.h2``;
+
+const List = styled.ul``;
+
+const ListItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 0 10px 0;
+`;
+
+const ToDoItem = styled.p`
+  margin: 0px;
+`;
+
+const Button = styled.button`
+  height: 20px;
+  margin: 0 20px;
+  min-width: 140px;
+`;
